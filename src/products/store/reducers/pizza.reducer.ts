13006c7c66c23c0731e4ef69a245cf9c804a22bd
tony walt <tony.walt@charter.com>
2018-09-18 15:10:53 -0600

    added first effect

--- src/products/store/reducers/pizza.reducer.ts
+++ src/products/store/reducers/pizza.reducer.ts
@@ -2,44 +2,13 @@ import * as fromPizzas from "../actions/pizza.actions";
 import { Pizza } from "../../models/pizza.model";
 
 export interface PizzaState {
-  data: Pizza[];
+  entities: { [id: number]: Pizza };
   loaded: boolean;
   loading: boolean;
 }
 
 export const initialState: PizzaState = {
-  data: [
-    {
-      "name": "Blazin' Inferno",
-      "toppings": [
-        {
-          "id": 10,
-          "name": "pepperoni"
-        },
-        {
-          "id": 9,
-          "name": "pepper"
-        },
-        {
-          "id": 3,
-          "name": "basil"
-        },
-        {
-          "id": 4,
-          "name": "chili"
-        },
-        {
-          "id": 7,
-          "name": "olive"
-        },
-        {
-          "id": 2,
-          "name": "bacon"
-        }
-      ],
-      "id": 1
-    }
-  ],
+  data: [],
   loaded: false,
   loading: false
 };
@@ -48,16 +17,19 @@ export function reducer(
   state = initialState,
   action: fromPizzas.PizzasAction
 ): PizzaState {
-  let registry = {
-    [fromPizzas.LOAD_PIZZAS] : () => { return {...state, loading: true}; },
-    [fromPizzas.LOAD_PIZZAS_SUCCESS] : () => { return {...state, loading: false, loaded: true}; },
-    [fromPizzas.LOAD_PIZZAS_FAIL] : () => { return {...state, loading: false, loaded: false}; }
+
+  switch(action.type) {
+    case fromPizzas.LOAD_PIZZAS:
+      return {...state, loading: true};
+
+    case fromPizzas.LOAD_PIZZAS_SUCCESS:
+      const data = action.payload;
+      return {...state, loading: false, loaded: true, data};
+
+    case fromPizzas.LOAD_PIZZAS_FAIL:
+      return {...state, loading: false, loaded: false};
   }
-  if (registry[action.type]) {
-    return registry[action.type]();
-  } else {
-    return state;
-  }
+  return state;
 }
 
 export const getPizzasLoading = (state: PizzaState) => state.loading;
